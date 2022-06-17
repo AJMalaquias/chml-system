7288f135d837c5273c40a6085077e9cb79eaed73
Anselmo Malaquias <40340096+AJMalaquias@users.noreply.github.com>
2022-06-17 13:17:54 +0100

    versão actualizada

--- src/entidades/enumeradores/Sexo.java
+++ src/entidades/enumeradores/Sexo.java
@@ -1,2 +1,22 @@
-package entidades.enumeradores;public enum Sexo {
+package entidades.enumeradores;
+
+public enum Sexo {
+    MASCULINO(1, "Masculino"),
+    FEMININO(2, "Feminino");
+
+    private Integer indice;
+    private String valor;
+
+    Sexo(Integer indice, String valor) {
+        this.indice = indice;
+        this.valor = valor;
+    }
+
+    public Integer getIndice() {
+        return indice;
+    }
+
+    public String getValor() {
+        return valor;
+    }
 }
